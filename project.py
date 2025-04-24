bd65b4e2d1254b4db396d19b46aad91671c03158
Abhishek95100 <abhishekverma95100@gmail.com>
2025-04-24 18:11:20 +0530

    Add files via upload

---
+++ project.py
@@ -0,0 +1,19 @@
+marks = int (input("enter student marks:"))
+
+if (marks>= 90):
+    grade = "A"
+    
+elif(marks >= 80 and marks <90):
+    grade = "b"
+
+elif( marks >=70 and  marks<80 ):
+    grade = "c"
+
+else :
+    grade = "D"
+
+    print("grade of the student ->", grade)
+
+
+ 
+
